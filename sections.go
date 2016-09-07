eae853ea29f2090a84a24444df4bab9f1cecbb0a
Peter Bale <peter@livlt0229994.bskyb.com>
2016-09-07 10:35:01 +0100

    Added get section method

--- sections.go
+++ sections.go
@@ -20,6 +20,19 @@ type SectionsData struct {
   Description string `json:"description"`
 }
 
+type Section struct {
+  Code    int `json:"code"`
+  Success bool `json:"success"`
+  Data    SectionsData `json:"data"`
+  Message string `json:"message"`
+}
+
+type SectionData struct {
+  Id          string `json:"id"`
+  Name        string `json:"name"`
+  Description string `json:"description"`
+}
+
 type SectionsSubnets struct {
   Code    int `json:"code"`
   Success bool `json:"success"`
@@ -54,6 +67,29 @@ func GetSections(server_url string, application string, token string) (*Sections
     return sectionsData, errors.New(sectionsData.Message)
   }
   return sectionsData, nil
+}
+
+func GetSection(server_url string, application string, sectionId string, token string) (*Sections, error) {
+  var sectionData = new(Section)
+  client := &http.Client{}
+  req, err := http.NewRequest("GET", "https://" + server_url + "/api/" + application + "/sections/" + sectionId + "/", nil)
+  req.Header.Add("token", token)
+  resp, err := client.Do(req)
+  if (err!=nil) {
+    return sectionData, err
+  }
+  body, err := ioutil.ReadAll(resp.Body)
+  if (err!=nil) {
+    return sectionData, err
+  }
+  err = json.Unmarshal([]byte(body), &sectionData)
+  if(err != nil){
+    return sectionData, err
+  }
+  if sectionData.Code != 200 {
+    return sectionData, errors.New(sectionData.Message)
+  }
+  return sectionData, nil
 }
 
 func GetSectionsSubnets(server_url string, application string, sectionId string, token string) (*SectionsSubnets, error) {
